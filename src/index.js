fed9ffac6dbdb7030bcf26f1d7d3bd8761f3f6d4
Riccardo <rbaldini10@yahoo.it>
2022-01-13 20:34:14 +0100

    dgr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Projects/01-birthday-reminder/src/App";
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
